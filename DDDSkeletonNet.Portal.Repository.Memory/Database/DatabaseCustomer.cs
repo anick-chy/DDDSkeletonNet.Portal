0384ff77b23023c9495bf0c81d5a4569a08d1cc6
anick-chy <anickchowdhury@gmail.com>
2015-11-24 02:58:18 +0600

    ObjectContext Class
    Created ObjectContext for InMemory Database

---
+++ DDDSkeletonNet.Portal.Repository.Memory/Database/DatabaseCustomer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DDDSkeletonNet.Portal.Repository.Memory.Database
+{
+    public class DatabaseCustomer
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Telephone { get; set; }
+    }
+}
---
+++ DDDSkeletonNet.Portal.Repository.Memory/Repository.cs
@@ -0,0 +1,30 @@
+﻿using DDDSkeletonNet.Infrastructure.Common.Domain;
+using DDDSkeletonNet.Infrastructure.Common.UnitOfWork;
+using System;
+
+namespace DDDSkeletonNet.Portal.Repository.Memory
+{
+    public abstract class Repository<DomainType, IdType, DatabaseType> : IUnitOfWorkRepository where DomainType : IAggregateRoot
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public Repository(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public void PersistInsertion(IAggregateRoot aggregateRoot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PersistUpdate(IAggregateRoot aggregateRoot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PersistDeletion(IAggregateRoot aggregateRoot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
