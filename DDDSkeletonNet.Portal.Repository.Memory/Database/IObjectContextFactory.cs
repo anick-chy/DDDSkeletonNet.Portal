c788f4051338e3fd486f0223792d6d22da7393a2
anick-chy <anickchowdhury@gmail.com>
2015-11-24 19:33:40 +0600

    object factory
    Created Object Factory

---
+++ DDDSkeletonNet.Portal.Repository.Memory/Database/IObjectContextFactory.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DDDSkeletonNet.Portal.Repository.Memory.Database
+{
+    public interface IObjectContextFactory
+    {
+        InMemoryDatabaseObjectContext Create();
+    }
+}
