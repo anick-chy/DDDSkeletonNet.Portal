5f264c6874637b87e26deb4549af841592fc3f29
anick-chy <anickchowdhury@gmail.com>
2016-01-12 02:51:58 +0600

    Services Started

---
+++ DDDSkeletonNet.Portal.ApplicationServices/ModelConversions/ConversionHelper.cs
@@ -0,0 +1,25 @@
+﻿using DDDSkeleton.Portal.Domain.Customer;
+using DDDSkeletonNet.Portal.ApplicationServices.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DDDSkeletonNet.Portal
+{
+    public static class ConversionHelper
+    {
+        public static CustomerViewModel ConvertToViewModel(this Customer customer)
+        {
+            return new CustomerViewModel()
+            {
+                AddressLine1 = customer.CustomerAddress.AddressLine1,
+                AddressLine2 = customer.CustomerAddress.AddressLine2,
+                City = customer.CustomerAddress.City,
+                PostalCode = customer.CustomerAddress.PostalCode,
+                Id = customer.Id,
+                Name = customer.Name
+            };
+        }
+    }
+}
